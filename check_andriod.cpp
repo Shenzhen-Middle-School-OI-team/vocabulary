36f2d1e81752057ad4d900870b3769766b6480cf
Steven Meng <52853137+GaisaiYuno@users.noreply.github.com>
2020-07-28 11:51:16 +0800

    Update check_andriod.cpp

--- check_andriod.cpp
+++ check_andriod.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
-#include <vector>
 #include <cstring>
+#include <vector>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 #define MAXN 1000005
 using namespace std;
 inline int read(){
@@ -19,6 +21,11 @@ inline int read(){
 	}
 	return x*f;
 }
+
+const string attribute[]={"n","v","vt","vi","prep","adj","adv","abbr","pron"};
+const int attributeSize=9;
+const string space="___";
+
 bool isEmpty(string str){
 	for (int i=0;i<str.size();++i){
 		if (str[i]!='<'&&str[i]!=' '&&str[i]!='\n'&&str[i]!='#') return false;
@@ -26,65 +33,132 @@ bool isEmpty(string str){
 	return true;
 }
 int type(string str){
-	if (str.size()>=2&&str[0]=='#'&&str[1]=='#') return 0;
-	else if (str.size()>=1&&str[0]=='>') return 1;
-	else return 2;
-}
-string explaination[MAXN],query[MAXN],answer[MAXN],full[MAXN];
-int order[MAXN];
-void Notice(string note){
-	cout<<note<<endl;
+	if (str.size()>=3&&str[0]=='#'&&str[1]=='#'&&str[2]!='#') return 0;
+	else if (str.size()>=4&&str[0]=='#'&&str[1]=='#'&&str[2]=='#'&&str[3]!='#') return 1;
+	else if (str.size()>=1&&str[0]=='>') return 2;
+	else return 3;
 }
 bool isAlphabet(char c){
+	if (c=='\'') return true;
+	if (c=='…') return true;
+	if (c=='(') return true;
+	if (c==')') return true;
 	return ('a'<=c&&c<='z')||('A'<=c&&c<='Z');
 }
+
+string _explaination[MAXN],_query[MAXN],_answer[MAXN],_full[MAXN];
+string explaination[MAXN],query[MAXN],answer[MAXN],full[MAXN];
+int order[MAXN],belong[MAXN],choosed[MAXN];
+
 void parse(int index,string str){
-	bool flag=false,need=false;
-	string ans="",ask=str;
-	for (int i=0;i<ask.size();++i){
-		bool f=isAlphabet(ask[i]);
-		if (need&&!f) break;
-		if (f){
-			need=true;
-			ans+=ask[i];
-			if (flag) ask[i]='_';
-			else flag=true;
+	string ask="",words="",word="";
+	bool head=true,use=true;
+	for (int i=0;i<str.size();++i){
+		if (!use){
+			ask+=str[i];
+			continue;
+		}
+		if (isAlphabet(str[i])){
+			word+=str[i];
+		}
+		else if (str[i]==' '||str[i]=='.'&&word.size()!=0){
+			for (int j=0;j<attributeSize;++j){
+				if (word==attribute[j]){
+					use=false;
+					break;
+				}
+			}
+			if (!use){
+				ask+=word;
+				ask+=str[i];
+				continue;
+			}
+
+			if (head) head=false;
+			else words+=" ";
+			words+=word;
+
+			ask+=word[0];
+			if (word.size()!=1) ask+=space;
+			ask+=" ";
+
+			word="";
+		}
+		else {
+			ask+=str[i];
 		}
 	}
-	answer[index]=ans;
-	query[index]=ask;
-	full[index]=str;
+	_answer[index]=words;
+	_query[index]=ask;
+	_full[index]=str;
 }
-ifstream words("words_utf8.md");
+ifstream words("英语必修一到必修十一_gb.md");
 ofstream wrong("wrong.md");
+string title[MAXN];
+int totTitle;
 int main(){
 	string nowLine="";
 	int last=0,tot=0;
+	totTitle=0;
 	while (getline(words,nowLine)){
 		if (isEmpty(nowLine)) continue;
 		int t=type(nowLine);
-		if (t==0) continue;
-		if (t==1) explaination[last]+=nowLine+"\n";
-		if (t==2) last=++tot,parse(tot,nowLine);
+		if (t==0){
+			totTitle++;
+			title[totTitle]=nowLine.substr(2);
+			continue;
+		}
+		if (t==1) continue;
+		if (t==2) _explaination[last]+=nowLine+"\n";
+		if (t==3) last=++tot,belong[last]=totTitle,parse(tot,nowLine);
 	}
+
 	printf("Read %d words\n",tot);
-	cerr<<"Read is ok.\nPlease input the full word.\nInput QUIT to quit.\nPress enter to continue."<<endl;
+	printf("Read is ok.\nPlease input the full word.\nInput QUIT to quit.\nPress enter to continue.\n");
 	getchar();
 	system("clear");
+
+	printf("Choose lists:\n");
+	for (int i=1;i<=totTitle;++i){
+		cout<<i<<":"<<title[i]<<endl;
+	}
+	printf("Enter tot choose:");
+	int totChoose;
+	scanf("%d",&totChoose);
+	for (int i=1;i<=totChoose;++i){
+		int choose;
+		scanf("%d",&choose);
+		choosed[choose]=true;
+	}
+	int all=0;
+	for (int i=1;i<=tot;++i){
+		if (choosed[belong[i]]){
+			++all;
+			explaination[all]=_explaination[all];
+			answer[all]=_answer[all];
+			full[all]=_full[all];
+			query[all]=_query[all];
+		}
+	}
+	getchar();getchar();
+	system("clear");
+
 	srand(time(NULL));
 	while (true){
-		for (int i=1;i<=tot;++i) order[i]=i;
-		random_shuffle(order+1,order+1+tot);
-		for (int i=1;i<=tot;++i){
+		for (int i=1;i<=all;++i) order[i]=i;
+		random_shuffle(order+1,order+1+all);
+		for (int i=1;i<=all;++i){
 			int index=order[i];
-			Notice(query[index]);
+			cout<<query[index]<<endl;
+
 			string user_in;
-			cin>>user_in;
-			if (user_in=="QUIT") break;
-			if (user_in==answer[index]) Notice("Correct!\n");
-			else Notice("Wrong!\n"),Notice(answer[index]),wrong<<full[index]<<endl,wrong<<explaination[index]<<endl;
-			Notice(explaination[index]);
-			getchar();
+			getline(cin,user_in);
+			if (user_in=="QUIT") return 0;
+
+			if (user_in==answer[index]) printf("Correct!\n");
+			else printf("Wrong!\n"),cout<<answer[index]<<endl,wrong<<full[index]<<endl,wrong<<explaination[index]<<endl;
+			cout<<explaination[index]<<endl;
+
 			getchar();
 			system("clear");
 		}
